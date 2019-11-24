5fef003024b01310ae29bb649fdf3c9ba6f30fe8
Yazdani <yaz4dani@gmail.com>
2019-11-25 02:43:35 +0600

    Initial commit

---
+++ lib/Home.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => new _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+
+      appBar: AppBar(
+        title: Text("Flutter Drag and Drop"),
+        backgroundColor: Colors.green,
+      ),
+
+      body: Container(
+        height: 800.0,
+        width: MediaQuery
+            .of(context)
+            .size
+            .width,
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: <Widget>[
+
+            Container(
+              height: 200.0,
+              margin: EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                  color: Colors.deepOrange,
+                borderRadius: BorderRadius.circular(15.0)
+              ),
+
+              child: Stack(
+                children: <Widget>[
+
+                  Positioned(
+                    top: 50.0,
+                    left: 50.0,
+                    bottom: 50.0,
+                    child: Text("Working here",
+                      style: TextStyle(
+                          fontSize: 25.0,
+                          color: Colors.white
+                      ),
+                    ),
+                  )
+
+                ],
+              ),
+            ),
+            SizedBox(width: 5.0,),
+
+            Container(
+              height: 200.0,
+              margin: EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                  color: Colors.deepOrange,
+                  borderRadius: BorderRadius.circular(15.0)
+              ),
+
+              child: Stack(
+                children: <Widget>[
+
+                  Positioned(
+                    top: 50.0,
+                    left: 50.0,
+                    bottom: 50.0,
+                    child: Text("Second Options",
+                      style: TextStyle(
+                          fontSize: 25.0,
+                          color: Colors.white
+                      ),
+                    ),
+                  )
+
+                ],
+              ),
+            ),
+            SizedBox(width: 5.0,),
+
+            Container(
+              height: 200.0,
+              margin: EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                  color: Colors.purple,
+                  borderRadius: BorderRadius.circular(15.0)
+              ),
+
+              child: Stack(
+                children: <Widget>[
+
+                  Positioned(
+                    top: 50.0,
+                    left: 50.0,
+                    bottom: 50.0,
+                    child: Text("Third Options",
+                      style: TextStyle(
+                          fontSize: 25.0,
+                          color: Colors.white
+                      ),
+                    ),
+                  )
+
+                ],
+              ),
+            ),
+            SizedBox(width: 5.0,),
+
+
+
+          ],
+        ),
+      ),
+
+
+    );
+  }
+}
+
+
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_drag_and_drop/Home.dart';
+
+void main(){
+  runApp(new MaterialApp(
+
+    home: new Home(),
+
+  ));
+}
+
+
+
+
